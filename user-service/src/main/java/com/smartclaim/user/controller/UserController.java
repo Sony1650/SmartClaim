d048bbd7de57b02b185b4525032e43ce8d78ed37
SruthiM <145879249+Sony1650@users.noreply.github.com>
2025-04-07 18:59:44 -0500

    Update UserController.java

--- user-service/src/main/java/com/smartclaim/user/controller/UserController.java
+++ user-service/src/main/java/com/smartclaim/user/controller/UserController.java
@@ -1,1 +1,31 @@
+package com.smartclaim.user.controller;
+
+import com.smartclaim.user.model.User;
+import com.smartclaim.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userService.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("Email already registered.");
+        }
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
 
