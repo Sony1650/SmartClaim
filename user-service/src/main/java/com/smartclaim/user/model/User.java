425880fe809ef9ad2c9d1f56afa8a1150bbfea11
SruthiM <145879249+Sony1650@users.noreply.github.com>
2025-04-07 16:53:51 -0500

    Update User.java

--- user-service/src/main/java/com/smartclaim/user/model/User.java
+++ user-service/src/main/java/com/smartclaim/user/model/User.java
@@ -1,1 +1,26 @@
+package com.smartclaim.user.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String role; // e.g., USER or ADMIN
+}
 
