b0689fe7308ad8b46caf1c05595df57e6cb07e4e
SruthiM <145879249+Sony1650@users.noreply.github.com>
2025-04-07 18:58:02 -0500

    Update UserService.java

--- user-service/src/main/java/com/smartclaim/user/service/UserService.java
+++ user-service/src/main/java/com/smartclaim/user/service/UserService.java
@@ -1,1 +1,34 @@
+package com.smartclaim.user.service;
+
+import com.smartclaim.user.model.User;
+import com.smartclaim.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        // Encrypt password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+}
 
